a7a939390f59032a4cdf2add1b38784e2448eb19
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2021-11-22 23:09:59 -0300

    Update CanvasConfigHandler.jsx

--- src/components/CanvasConfigHandler.jsx
+++ src/components/CanvasConfigHandler.jsx
@@ -1,12 +1,12 @@
-import React /*, { useState }*/ from 'react'
+import React, { useState } from 'react'
 
 function CanvasConfigHandler() {
 
-    //const [config, setConfig] = useState({ background: '#fff', color: '#000', widthLine: 5 }) 
+    const [config, setConfig] = useState({ background: '#fff', color: '#000', widthLine: 5 }) 
 
     return (
         <section className="canvasConfig">
-            
+            Seccion de configuracion
         </section>
     )
 }
